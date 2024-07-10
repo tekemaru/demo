d17f86c33a17ff03783bf5264b16abbcb8d0e581
tekemaru <dpm10198@gmail.com>
2024-07-10 14:49:44 +0900

    Initial commit

---
+++ demo/src/test/java/com/example/demo/controller/HelloControllerTest.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    void helloページにアクセスしたらhelloが返ること() throws Exception {
+        final var mvcResult = this.mockMvc.perform(get("/hello"))
+                .andDo(print()).andExpect(status().isOk()).andReturn();
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content, "hello");
+    }
+}
